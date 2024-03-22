e3327b5bf47ed6a555fbdc2085fbe106f4b383af
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2024-03-22 13:58:57 +0200

    Updates

--- static/js/256.c221d456.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcamper_rental_ua=self.webpackChunkcamper_rental_ua||[]).push([[256],{256:(a,e,r)=>{r.r(e),r.d(e,{default:()=>l});r(43);var c=r(579);const l=()=>(0,c.jsx)("div",{children:"Catalog"})}}]);
-//# sourceMappingURL=256.c221d456.chunk.js.map
